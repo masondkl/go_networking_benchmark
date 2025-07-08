ec6da98e275138b626bcdefb529813ee60f90f8c
Mason <masondunkle@gmail.com>
2025-07-08 14:35:43 -0400

    testing

--- server/peers.go
+++ server/peers.go
@@ -17,7 +17,7 @@ func (s *Server) processMessages(msgs []raftpb.Message) {
 	for _, msg := range msgs {
 		go func() {
 			buffer := s.pool.Get().([]byte)
-			shared.GrowSlice(buffer, uint32(msg.Size())+4)
+			buffer = shared.GrowSlice(buffer, uint32(msg.Size())+4)
 			size, err := msg.MarshalTo(buffer[4:])
 			if err != nil {
 				return
