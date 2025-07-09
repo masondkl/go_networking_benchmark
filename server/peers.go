9cf5315550a926eca289ea1b1b2c9ac27a61f652
purp <lucasphone75@gmail.com>
2025-07-09 01:09:19 -0600

    remove batching

--- server/peers.go
+++ server/peers.go
@@ -14,44 +14,65 @@ import (
 )
 
 func (s *Server) processMessages(msgs []raftpb.Message) {
-	grouped := make(map[uint64][]raftpb.Message)
-
-	for _, m := range msgs {
-		grouped[m.To] = append(grouped[m.To], m)
-	}
-
-	for to, group := range grouped {
-		go func(to uint64, group []raftpb.Message) {
-			var offset = 8
+	for _, msg := range msgs {
+		go func() {
 			buffer := s.pool.Get().([]byte)
-			for i := range group {
-				offset += group[i].Size() + 4
+			buffer = shared.GrowSlice(buffer, uint32(msg.Size())+4)
+			size, err := msg.MarshalTo(buffer[4:])
+			if err != nil {
+				return
 			}
-			fmt.Printf("We need to grow to: offset=%d for messages=%d\n", offset, len(msgs))
-			buffer = shared.GrowSlice(buffer, uint32(offset))
-			offset = 8
-			for i := range group {
-				msg := group[i]
-				size, err := msg.MarshalTo(buffer[offset+4:])
-				if err != nil {
-					return
-				}
-				binary.LittleEndian.PutUint32(buffer[offset:offset+4], uint32(size))
-				offset += size + 4
-			}
-			binary.LittleEndian.PutUint32(buffer[0:4], uint32(offset-4))
-			binary.LittleEndian.PutUint32(buffer[4:8], uint32(len(group)))
-			peerIdx := to - 1
+			binary.LittleEndian.PutUint32(buffer[:4], uint32(size))
+			peerIdx := msg.To - 1
 			connIdx := atomic.AddUint32(&s.peerConnRoundRobins[peerIdx], 1) % uint32(s.flags.NumPeerConnections)
 			peer := s.peerConnections[peerIdx][connIdx]
 			peer.WriteLock.Lock()
-			if err := shared.Write(*peer.Connection, buffer[:offset]); err != nil {
-				log.Printf("Write error to peer %d: %v", to, err)
+			if err := shared.Write(*peer.Connection, buffer[:size+4]); err != nil {
+				log.Printf("Write error to peer %d: %v", msg.To, err)
 			}
 			peer.WriteLock.Unlock()
 			s.pool.Put(buffer)
-		}(to, group)
+		}()
 	}
+	//
+	//grouped := make(map[uint64][]raftpb.Message)
+	//
+	//for _, m := range msgs {
+	//	grouped[m.To] = append(grouped[m.To], m)
+	//}
+	//
+	//for to, group := range grouped {
+	//	go func(to uint64, group []raftpb.Message) {
+	//		var offset = 8
+	//		buffer := s.pool.Get().([]byte)
+	//		for i := range group {
+	//			offset += group[i].Size() + 4
+	//		}
+	//		fmt.Printf("We need to grow to: offset=%d for messages=%d\n", offset, len(msgs))
+	//		buffer = shared.GrowSlice(buffer, uint32(offset))
+	//		offset = 8
+	//		for i := range group {
+	//			msg := group[i]
+	//			size, err := msg.MarshalTo(buffer[offset+4:])
+	//			if err != nil {
+	//				return
+	//			}
+	//			binary.LittleEndian.PutUint32(buffer[offset:offset+4], uint32(size))
+	//			offset += size + 4
+	//		}
+	//		binary.LittleEndian.PutUint32(buffer[0:4], uint32(offset-4))
+	//		binary.LittleEndian.PutUint32(buffer[4:8], uint32(len(group)))
+	//		peerIdx := to - 1
+	//		connIdx := atomic.AddUint32(&s.peerConnRoundRobins[peerIdx], 1) % uint32(s.flags.NumPeerConnections)
+	//		peer := s.peerConnections[peerIdx][connIdx]
+	//		peer.WriteLock.Lock()
+	//		if err := shared.Write(*peer.Connection, buffer[:offset]); err != nil {
+	//			log.Printf("Write error to peer %d: %v", to, err)
+	//		}
+	//		peer.WriteLock.Unlock()
+	//		s.pool.Put(buffer)
+	//	}(to, group)
+	//}
 }
 
 func (s *Server) handlePeerConnection(conn net.Conn) {
@@ -74,21 +95,16 @@ func (s *Server) handlePeerConnection(conn net.Conn) {
 		if err := shared.Read(conn, readBuffer[:totalSize]); err != nil {
 			return
 		}
-		msgCount := binary.LittleEndian.Uint32(readBuffer[:4])
-		offset := uint32(4)
-		for i := uint32(0); i < msgCount; i++ {
-			size := binary.LittleEndian.Uint32(readBuffer[offset : offset+4])
-			var msg raftpb.Message
-			if err := msg.Unmarshal(readBuffer[offset+4 : offset+4+size]); err != nil {
-				panic(fmt.Sprintf("Error unmarshaling message: %v", err))
-			}
-			offset += size + 4
-			go func() {
-				if err := s.node.Step(context.TODO(), msg); err != nil {
-					log.Printf("Step error: %v", err)
-				}
-			}()
+
+		var msg raftpb.Message
+		if err := msg.Unmarshal(readBuffer[:totalSize]); err != nil {
+			panic(fmt.Sprintf("Error unmarshaling message: %v", err))
 		}
+		go func() {
+			if err := s.node.Step(context.TODO(), msg); err != nil {
+				log.Printf("Step error: %v", err)
+			}
+		}()
 	}
 }
 
