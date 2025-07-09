9c3c1c6292d8d6ab051dc0804e2b0cf9023b460f
purp <lucasphone75@gmail.com>
2025-07-09 03:24:18 -0600

    Revert "remove batching"
    This reverts commit 1406e286a7db57fd805493259b4772fe0a6397eb.

--- server/peers.go
+++ server/peers.go
@@ -110,7 +110,7 @@ func (s *Server) handlePeerConnection(conn net.Conn) {
 			panic(fmt.Sprintf("Error unmarshaling message: %v", err))
 		}
 
-		fmt.Printf("Received peer message: %d, %d, %d\n", len(msg.Entries), msg.Index, msg.From)
+		fmt.Printf("Received peer message: %d, %d\n", len(msg.Entries), msg.Index)
 
 		go func() {
 			if err := s.node.Step(context.TODO(), msg); err != nil {
