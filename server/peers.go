01e065db46619f7d4a51fca9cf0fa6361c3c83b4
purp <lucasphone75@gmail.com>
2025-07-09 03:24:24 -0600

    Revert "debugs"
    This reverts commit 59db8adf7d83f92dc118fa49071c634f9c97ab76.

--- server/peers.go
+++ server/peers.go
@@ -14,11 +14,34 @@ import (
 )
 
 func (s *Server) processMessages(msgs []raftpb.Message) {
-	grouped := make(map[uint64][]raftpb.Message)
+	//for _, msg := range msgs {
+	//	go func() {
+	//		buffer := s.pool.Get().([]byte)
+	//		buffer = shared.GrowSlice(buffer, uint32(msg.Size())+4)
+	//		size, err := msg.MarshalTo(buffer[4:])
+	//		if err != nil {
+	//			return
+	//		}
+	//		binary.LittleEndian.PutUint32(buffer[:4], uint32(size))
+	//		peerIdx := msg.To - 1
+	//		connIdx := atomic.AddUint32(&s.peerConnRoundRobins[peerIdx], 1) % uint32(s.flags.NumPeerConnections)
+	//		peer := s.peerConnections[peerIdx][connIdx]
+	//		peer.WriteLock.Lock()
+	//		if err := shared.Write(*peer.Connection, buffer[:size+4]); err != nil {
+	//			log.Printf("Write error to peer %d: %v", msg.To, err)
+	//		}
+	//		peer.WriteLock.Unlock()
+	//		s.pool.Put(buffer)
+	//	}()
+	//}
+
+	var grouped = make(map[uint64][]raftpb.Message)
 
 	for _, m := range msgs {
 		grouped[m.To] = append(grouped[m.To], m)
 	}
+
+	fmt.Printf("Processing %d messages\n", len(msgs))
 
 	for to, group := range grouped {
 		go func(to uint64, group []raftpb.Message) {
@@ -62,7 +85,7 @@ func (s *Server) handlePeerConnection(conn net.Conn) {
 		return
 	}
 	peerIndex := binary.LittleEndian.Uint32(bytes)
-	fmt.Printf("Peer index: %d\n", peerIndex)
+	log.Printf("Got connection from peer %d", peerIndex)
 
 	readBuffer := make([]byte, 10000000)
 	for {
@@ -70,6 +93,7 @@ func (s *Server) handlePeerConnection(conn net.Conn) {
 			return
 		}
 		totalSize := binary.LittleEndian.Uint32(readBuffer[:4])
+		fmt.Printf("Total size: %d\n", totalSize)
 		readBuffer = shared.GrowSlice(readBuffer, totalSize)
 		if err := shared.Read(conn, readBuffer[:totalSize]); err != nil {
 			return
