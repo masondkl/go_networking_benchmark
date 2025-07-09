f1f590677ddc853da8eb2618efcdc7ea19072755
purp <lucasphone75@gmail.com>
2025-07-09 02:56:11 -0600

    remove batching

--- server/clients.go
+++ server/clients.go
@@ -141,6 +141,7 @@ func (s *Server) respondToClient(op byte, id uuid.UUID, data []byte) {
 		request := senderAny.(shared.PendingRead)
 
 		length := uint32(9 + len(data))
+		fmt.Printf("Responding to client write size: size=%d\n", length)
 		buffer = shared.GrowSlice(buffer, length)
 		binary.LittleEndian.PutUint32(buffer[:4], length-4)
 		buffer[4] = op
