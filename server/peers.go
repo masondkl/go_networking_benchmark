2757773db5c9bec7cd0cd75c5695b4aacbd346c0
purp <lucasphone75@gmail.com>
2025-07-10 00:12:38 -0600

    swap to custom peer forwarding

--- server/peers.go
+++ server/peers.go
@@ -143,7 +143,7 @@ func (s *Server) handlePeerConnection(conn net.Conn) {
 		op := readBuffer[0]
 		//fmt.Printf("Got op: %d\n", op)
 		if op == shared.OP_FORWARD {
-			fmt.Printf("Got forward from peer %d\n", peerIndex)
+			fmt.Printf("Got forward from peer %d, %d\n", peerIndex, totalSize)
 			dataCopy := make([]byte, totalSize)
 			copy(dataCopy, readBuffer[:totalSize])
 			s.proposeChannel <- func() {
