20831f3251e7dc2482e7dc5b30acb1b499597e09
purp <lucasphone75@gmail.com>
2025-07-10 00:03:02 -0600

    swap to custom peer forwarding

--- server/clients.go
+++ server/clients.go
@@ -73,6 +73,7 @@ func (s *Server) handleClientMessage(client shared.Client, data []byte) {
 
 	size := len(data) + 21
 	s.senders.Store(messageId, client)
+	fmt.Printf("Leader: %d - %d\n", s.leader, s.config.ID)
 	if s.leader-1 == ownerId {
 		dataCopy := make([]byte, size)
 		dataCopy[0] = shared.OP_MESSAGE
